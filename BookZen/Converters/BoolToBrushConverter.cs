981316616782437f8b4846c821f96f140178432d
Jakub Czartek <kuba@linux.pl>
2020-07-01 09:44:03 +0200

    Dodanie konwerta BoolToBrushConverter

---
+++ BookZen/Converters/BoolToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BookZen.Converters
+{
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true && parameter is Color color)
+                return new SolidColorBrush(color);
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
