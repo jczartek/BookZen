ab0fd8b30bd7015e525453eab20272f80822d7fa
Jakub Czartek <kuba@linux.pl>
2020-09-27 20:05:27 +0200

    Delete code

--- ServiceLayer/ServiceFactory.cs
+++ ServiceLayer/ServiceFactory.cs
@@ -1,5 +1,4 @@
 ﻿using RepositoryLayer;
-using ServiceLayer.Abstract;
 using ServiceLayer.Concrete;
 using System;
 
@@ -7,7 +6,7 @@ namespace ServiceLayer
 {
     public static class ServiceFactory
     {
-        public static  IBookService CreateBookService()
+        public static IBookService CreateBookService()
         {
             return new BookService(RepositoryFactory.CreateBookRepository());
         }
@@ -28,7 +27,7 @@ namespace ServiceLayer
             }
         }
 
-        public static TResult CreateBookService<TResult>(Func<IBookService,TResult> serve)
+        public static TResult CreateBookService<TResult>(Func<IBookService, TResult> serve)
         {
             using (var service = CreateBookService())
             {
@@ -39,43 +38,6 @@ namespace ServiceLayer
         public static TResult CreateBookService<TResult, TArg>(Func<IBookService, TArg, TResult> serve, TArg arg)
         {
             using (var service = CreateBookService())
-            {
-                return serve(service, arg);
-            }
-        }
-
-        public static IBookRentalService CreateBookRentalService()
-        {
-            return new BookRentalService(RepositoryFactory.CreateBookRentalRepository());
-        }
-
-        public static void CreateBookRentalService(Action<IBookRentalService> serve)
-        {
-            using (var service = CreateBookRentalService())
-            {
-                serve(service);
-            }
-        }
-
-        public static void CreateBookRentalService<TArg>(Action<IBookRentalService, TArg> serve, TArg arg)
-        {
-            using (var service = CreateBookRentalService())
-            {
-                serve(service, arg);
-            }
-        }
-        
-        public static TResult CreateBookRentalService<TResult>(Func<IBookRentalService, TResult> serve)
-        {
-            using (var service = CreateBookRentalService())
-            {
-                return serve(service);
-            }
-        }
-
-        public static TResult CreateBookRentalService<TResult, TArg>(Func<IBookRentalService, TArg, TResult> serve, TArg arg)
-        {
-            using (var service = CreateBookRentalService())
             {
                 return serve(service, arg);
             }
